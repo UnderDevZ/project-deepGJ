d2897ecf548d6e5ad61be431d5d4ede58f4e05aa
DSRKami <pintishere@gmail.com>
2023-08-25 17:24:51 +0100

    Level 1 Refined and Menu Stuff
    Blergh

---
+++ Assets/_Scripts/Main_Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level 1");
+        Debug.Log("Loading Level 1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game");
+        Application.Quit();
+    }
+}
